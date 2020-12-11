defc9581f7e7aa851c1980ce70fb127468338de0
Wilson Assis <wilson.assis@fatec.sp.gov.br>
2020-12-11 07:56:46 -0300

    Notebook de treinamento de modelo

--- utilidades/caminho_projeto.py
+++ utilidades/caminho_projeto.py
@@ -1,6 +1,6 @@
 import os
 import utilidades.tratamento_erros as te
-
+from sys import modules
 
 # CONSTANTES
 _CAMINHO_MODULO = 'simulacao.caminho_projeto.'
@@ -12,12 +12,20 @@ def __caminho_projeto():
   :return: string do caminho obtido.
   """
   try:
-    caminho = os.getcwd().replace('\\', r'/')
-    fim = caminho.find('projeto_cangaco') + len('projeto_cangaco')
-    return caminho[:fim] + '/'
+    rodando_google_colab = 'google.colab' in modules
+
+    if rodando_google_colab:
+      caminho = '/content/drive/My Drive/workspace/Projetos/projeto_cangaco/'
+
+    else:
+      caminho = os.getcwd().replace('\\', r'/')
+      fim = caminho.find('projeto_cangaco') + len('projeto_cangaco')
+      caminho = caminho[:fim] + '/'
+
+    return caminho
 
   except BaseException as erro:
-    te.base_exception(erro, _CAMINHO_MODULO + 'caminho_projeto')
+    te.base_exception(erro, _CAMINHO_MODULO + '__caminho_projeto')
 
 
 CAMINHO_PROJETO = __caminho_projeto()
